a3e1a984658a2ef6acb43c7918f11e79bcbc7c60
Harrison Glover <harrison_glover@yahoo.com>
2020-11-09 18:16:33 -0600

    added logic to calculate calories burned

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ function calculateCalories(){
 	$.each($(".list-title").children(), function(index,value){
 		var addCalories = $(value).text();
 		var calories = addCalories.match(/\d+/g).map(Number)
-		// console.log(calories);
-		// console.log(parseInt(addCalories));
-		// calorieArr.push(calories[0])
 		calorieCount += calories[0];
 	})
 	console.log(calorieCount);
+	$("#total").append(" " ,calorieCount," "," "," ");
+	
 }
 function foodChoice() {
 	var foodChoice = $(".food-choice").val().trim();
@@ -24,38 +23,70 @@ function foodChoice() {
 			"x-rapidapi-host": "nutritionix-api.p.rapidapi.com"
 		}
 	};
-
+	// var totalCalories = calorieCount;
 	$.ajax(settings).done(function (response) {
 		console.log(response);
-		// var divEl = $("<div>").attr("id", "calories").append(foodChoice + ":" + " " + response.hits[0].fields.nf_calories + " calories");
-		// $(".box").append(divEl);
+		
 		var liEl = $("<li>").append(foodChoice + ":" + " " + response.hits[0].fields.nf_calories + " calories");
-		var addButton = $("<button>").addClass("add-btn").text("Add to List");
-		$("#food-list").prepend(liEl,addButton)	;
-		// var clearButton = $("<button>").addClass("reset-btn").text("Reset");
-
-		// $("#clear").append(clearButton);
+		
+		$(".list-title").prepend(liEl)	;
 		
 		
 
 
 	});
 }
-$(document).on("click",".add-btn", function(){
-	var listText = $(this).siblings()[0];
-	$(".list-title").append(listText);
-	$(this).remove();	
+
+function caloriesBurned(){
+	// var weight = $("#weight-input").val()
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://fitness-calculator.p.rapidapi.com/mets",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-key": "250391abd2msh4726f31e05ef85bp1fff09jsnbc63edbdae23",
+			"x-rapidapi-host": "fitness-calculator.p.rapidapi.com"
+		}
+	};
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+		var weight = parseInt(localStorage.getItem("weight"));
+		console.log(weight);
+		var bikingMets = response["bicycling"]["activity 8"]["MET"];
+		console.log (bikingMets);
+		var joggingMets = response["running"]["activity 473"]["MET"];
+		console.log(joggingMets);
+		var bikingCalorie30min = bikingMets * weight * (.454 * 3.5)/200* 30; 
+		$("#total").append("",bikingCalorie30min);
+	});
+}
+
+$(document).on("click","#total-calories", function(){
+	
 	calculateCalories();
+	caloriesBurned();
+
 
 
 
 	
 })
 $("#clear-btn").on("click", function(){
-	$("#food-list").empty();
+	$(".list-title").empty();
+	$("#total").empty();
+
 })
 
-$(".submit-button").on("click", function (event) {
+$(".add-to-list").on("click", function (event) {
 	event.preventDefault();
 	foodChoice();
+})
+$(".submit-button").on("click", function(event){
+	event.preventDefault()
+	var weight = $(".weight").val().trim();
+	localStorage.setItem("weight", weight);
+	
+	
 })
