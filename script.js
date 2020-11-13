490a09f1f108b7820ad0a06eb79c70c4ea2481ec
Harrison Glover <harrison_glover@yahoo.com>
2020-11-12 20:36:30 -0600

    corrected error preventing calores that were deleted form being removed from total calories

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 // var foodName = $(".food-choice").val().trim()
 // console.log(foodName);
-function calculateCalories(){
+// [
+// 	{
+// 		foodId:
+// 		calories
+// 	}
+// 	{
+// 		foodId:
+// 		calories
+// 	}
+// 	{
+// 		foodId:
+// 		calories
+// 	}
+// 	{
+// 		foodId:
+// 		calories
+// 	}			
+// ]
+var foods = [];
+function calculateCalories(shouldSubtract){
 	var calorieCount = 0;
-	$.each($(".list-title").children(), function(index,value){
-		var addCalories = $(value).text();
-		var calories = addCalories.match(/\d+/g).map(Number)
-		calorieCount += calories[0];
-	})
+	// $.each($(".list-title").children(), function(index,value){
+	// 	var addCalories = $(value).text();
+	// 	console.log(addCalories);
+	// 	var calories = addCalories.match(/\d+/g).map(Number)
+	// 	calorieCount += calories[0];
+	// })
+	for (var i = 0; i < foods.length; i++){
+		console.log(foods[i].calories);
+			calorieCount += foods[i].calories;
+	}
 	console.log(calorieCount);
-	$("#total").append(" " ,calorieCount," "," "," ");
+	$("#total").text (calorieCount);
+	localStorage.setItem("calories", calorieCount);
+
 	
 }
 function foodChoice() {
@@ -26,12 +52,22 @@ function foodChoice() {
 	// var totalCalories = calorieCount;
 	$.ajax(settings).done(function (response) {
 		console.log(response);
-		var itemDiv = $("<div>").attr("style", "display: flex; justify-content: space-between; width: 70%;")
+		var itemDiv = $("<div>").attr("style", "display: flex; justify-content: space-between; width: 70%").attr("id",response.hits[0]._id);
 		var resetButton = $("<button>").addClass("delete-btn").text("delete");
 		var liEl = $("<div>").text(foodChoice + ":" + " " + response.hits[0].fields.nf_calories + " calories").addClass("li-tag");
 		itemDiv.append(liEl, resetButton)
 		$(".list-title").prepend(itemDiv)
 		console.log(liEl);
+		foods.push({foodId: response.hits[0]._id, calories: response.hits[0].fields.nf_calories});
+		// console.log(foods);
+		var foodInput = $(".food-choice").val();
+		// var weightInput = $("#weight-text").val();
+		if(!foodInput){
+			alert("Must enter a value");
+			return;	
+		};
+		
+		calculateCalories();
 		
 		
 
@@ -95,22 +131,24 @@ function caloriesBurned(){
 	});
 }
 
-$(document).on("click","#total-calories", function(){
-	var foodInput = $(".food-choice").val();
-	// var weightInput = $("#weight-text").val();
-	if(!foodInput){
-		alert("Must enter a value");
-		return;	
-	};
+
+
+// $(document).on("click","#total-calories", function(){
+// 	var foodInput = $(".food-choice").val();
+// 	// var weightInput = $("#weight-text").val();
+// 	if(!foodInput){
+// 		alert("Must enter a value");
+// 		return;	
+// 	};
 	
-	calculateCalories();
-	// caloriesBurned();
+// 	calculateCalories();
+// 	// caloriesBurned();
 
 
 
 
 	
-})
+// })
 
 
 
@@ -135,9 +173,46 @@ $(document).on("click","#weight-submit-button", function(){
 })
 
 $(document).on("click",".delete-btn", function(){
+	// $(this).siblings().text	()
+	var removeFoodId = parseInt($(this).parent().attr("id"));
+	var removeFoodIndex = foods.findIndex(function(food){
+		return food.foodId === removeFoodId;
+
+
+	});
+	foods.splice(removeFoodIndex,1);
+	calculateCalories();
+	console.log(foods);
+	// array at beginning
+	// [
+	// 	{
+	// 		foodId: 0,
+	// 		calories: 345
+	// 	},
+	// 	{
+	// 		foodId: 1,
+	// 		calories: 345
+	// 	},
+	// 	{
+	// 		foodId: 2,
+	// 		calories: 345
+	// 	}				
+	// ]
+
+	// array upon deletion second item
+	// [
+	// 	{
+	// 		foodId: 0,
+	// 		calories: 345
+	// 	},
+	// 	{
+	// 		foodId: 2,
+	// 		calories: 345
+	// 	}				
+	// ]
 	$(this).siblings().remove();
 	$(this).remove();
-	calculateCalories();
+	
 })
 
 
@@ -152,13 +227,15 @@ $(".add-to-list").on("click", function (event) {
 	event.preventDefault();
 	foodChoice();
 })
-$(".submit-button").on("click", function(event){
-	event.preventDefault()
-	var weight = $(".weight").val().trim();
-	localStorage.setItem("weight", weight);
-	// $(".food-items").append(".list-title").content();
-	// console.log((".list-title").content())
+
+
+// $(".submit-button").on("click", function(event){
+// 	event.preventDefault()
+// 	var weight = $(".weight").val().trim();
+// 	localStorage.setItem("weight", weight);
+// 	// $(".food-items").append(".list-title").content();
+// 	// console.log((".list-title").content())
 	 
 	
 	
-})
+// })
